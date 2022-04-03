65dfb276665d3cc286d4f5a3c8440c86aa969dbe
Hamidul-1 <hamidul786cse@gmail.com>
2022-04-04 00:03:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Reviews from './components/Reviews/Reviews';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
             <h1>Header</h1>
+            <nav>
+                <Link to="/home">Home</Link>
+                <Link to="/reviews">Review</Link>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/blogs">Blogs</Link>
+                <Link to="/about">About</Link>  
+            </nav>
         </div>
     );
 };
