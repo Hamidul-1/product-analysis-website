5fdc8374216e2d89df0c71bf327ea4037f0ff6ac
Hamidul-1 <hamidul786cse@gmail.com>
2022-06-04 13:58:21 +0600

    added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <div>
             <div className='d-lg-flex p-5'>
                 <div className="heading">
-                    <h1>Apple Watch</h1>
+                    <h1>About Apple Watch</h1>
                     <p>Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness tracking, health-oriented capabilities, and wireless telecommunication, and integrates with iOS and other Apple products and services.</p>
                     <button className='btn btn-success px-3'>More Details</button>
                 </div>
